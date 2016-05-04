ed656b685bfb8ae22688de661aedb72ccaa42d7b
c4605 <bolasblack@gmail.com>
2016-05-05 00:08:25 +0800

    test

--- telegram-cli.sh
+++ telegram-cli.sh
@@ -1,9 +1,9 @@
 echo $PWD
 
 if [ -d ./telegram-cli-wrapper ]; then
-  git clone --recursive "https://github.com/bolasblack/telegram-cli-wrapper.git"
-else
   git submodule --recursive update
+else
+  git clone --recursive "https://github.com/bolasblack/telegram-cli-wrapper.git"
 fi
 
 cd ./telegram-cli-wrapper
