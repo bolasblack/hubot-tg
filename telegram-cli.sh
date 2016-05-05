5a947702b9c9023dcee7f88eb8158bdbc488c968
c4605 <bolasblack@gmail.com>
2016-05-05 11:03:16 +0800

    test

--- telegram-cli.sh
+++ telegram-cli.sh
@@ -1,8 +1,15 @@
-if [ -d ./telegram-cli-wrapper ]; then
-  git submodule --recursive update
-else
+if [ ! -d ./telegram-cli-wrapper ]; then
   git clone --recursive https://github.com/bolasblack/telegram-cli-wrapper.git
+  cd ./telegram-cli-wrapper
+  ./compile-dependencies.sh
+  exit
 fi
 
 cd ./telegram-cli-wrapper
-./compile-dependencies.sh
+originHash=$(git rev-parse HEAD)
+git fetch origin && git reset --hard origin/master
+git submodule update --init --recursive
+currentHash=$(git rev-parse HEAD)
+if [ $originHash != $currentHash ]; then
+  ./compile-dependencies.sh
+fi
